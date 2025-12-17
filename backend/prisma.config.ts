7a148e22a09f4900af1d4a31d1042eb527b1bd7e
Burhanuddin <burhanuddinrajkotwala11@gmail.com>
2025-12-17 23:04:13 +0530

    trying backend

---
+++ backend/prisma.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "prisma/config";
+
+export default defineConfig({
+  migrate: {
+    datasource: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
+  },
+});
