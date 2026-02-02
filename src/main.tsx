dd9a18e84552a37acc1a38dd6ef8baac862ec59a
BurhanRajkot <burhanuddinrajkotwala11@gmail.com>
2026-02-02 19:25:35 +0530

    adding dark and lgith mode on the website with a cool button

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { Auth0Provider } from '@auth0/auth0-react'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClient } from '@/lib/queryClient'
+import { ThemeProvider } from 'next-themes'
 
 import App from './App'
 import './index.css'
@@ -35,9 +36,11 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
           cacheLocation="localstorage"
           useRefreshTokens
         >
-          <FavoritesProvider>
-            <App />
-          </FavoritesProvider>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            <FavoritesProvider>
+              <App />
+            </FavoritesProvider>
+          </ThemeProvider>
         </Auth0Provider>
       </BrowserRouter>
     </QueryClientProvider>
